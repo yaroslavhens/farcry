cfa49cbd90b45f94cfca9e83cb5b5e89ebbfbad5
Yaroslav Hens <hens.yaroslav@gmail.com>
2018-11-19 18:46:03 +0200

    first commit

--- web/js/main.js
+++ web/js/main.js
@@ -1,19 +1,54 @@
 
-jQuery(document).ready(function($) {
-	$('.modal__tab').hide();
-    $('.modal__tab:first').show();
-	$('.head__conteiner div:first').addClass('active');
 
-	$('.head__conteiner div').click(function(event) {
-		$('.head__conteiner div').removeClass('active');
-		$(this).addClass('active')
-        $('.modal__tab').hide();
+ $(function(){
 
-		var selectTab = $(this).find('a').attr("href");
-		$(selectTab).fadeIn();
+    $('.block').on('click', function(){
 
-        $(function() {
-            $('.slider').slick();
+        $('.hide').slideToggle(300, function(){
+
+            if($(this).css('display') === 'none'){
+                $(this).removeAttr('style');
+            }
+
         });
+
     });
+
+     $('.block_2').on('click', function(){
+
+         $('.hide_2').slideToggle(300, function(){
+
+             if($(this).css('display') === 'none'){
+                 $(this).removeAttr('style');
+             }
+         });
+     });
+     $('.block_3').on('click', function(){
+
+         $('.hide_3').slideToggle(300, function(){
+
+             if($(this).css('display') === 'none'){
+                 $(this).removeAttr('style');
+             }
+         });
+     });
+     $('.block_4').on('click', function(){
+
+         $('.hide_4').slideToggle(300, function(){
+
+             if($(this).css('display') === 'none'){
+                 $(this).removeAttr('style');
+             }
+         });
+     });
+     $('.block_5').on('click', function(){
+
+         $('.hide_5').slideToggle(300, function(){
+
+             if($(this).css('display') === 'none'){
+                 $(this).removeAttr('style');
+             }
+         });
+     });
+
 });
